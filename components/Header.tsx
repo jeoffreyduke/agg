19035c03160e295b9e94961f7fe96a2d88c2ceac
jeoffreyduke <jeoffreyduke@gmail.com>
2022-11-02 21:21:07 +0100

    Reduced the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,42 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 480) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <header className={styles.Header}>
       <div className={styles.logo}>
-        <Image src="/assets/logo.svg" alt="logo" height={80} width={80} />
+        <Image
+          src="/assets/logo.svg"
+          alt="logo"
+          height={isMobile ? 40 : 80}
+          width={isMobile ? 40 : 80}
+        />
       </div>
 
       <nav className={styles.right}>
-        <div>STAKING</div>
-        <div>SOCIALS</div>
-        <div>PARTNERS</div>
-        <div>FAQ</div>
-        <button className={styles.btn}>WHITEPAPER</button>
+        {isMobile ? (
+          "Menu"
+        ) : (
+          <>
+            <div>STAKING</div>
+            <div>SOCIALS</div>
+            <div>PARTNERS</div>
+            <div>FAQ</div>
+            <button className={styles.btn}>WHITEPAPER</button>
+          </>
+        )}
       </nav>
     </header>
   );
