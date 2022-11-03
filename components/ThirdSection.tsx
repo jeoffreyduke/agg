439c923c42b26d5cefc75834e20bf0f3350d1a61
jeoffreyduke <jeoffreyduke@gmail.com>
2022-11-03 15:59:34 +0100

    made our partners section responsiveness

--- components/ThirdSection.tsx
+++ components/ThirdSection.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
@@ -19,6 +19,17 @@ const ThirdSection = () => {
   const decaTxt = useRef<HTMLDivElement>(null);
   const decaImg = useRef<HTMLDivElement>(null);
 
+  const [isSmallDevice, setIsSmallDevice] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 900) {
+      setIsSmallDevice(true);
+    } else {
+      setIsSmallDevice(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const openSponsors = (
     left: React.RefObject<HTMLDivElement>,
     right: React.RefObject<HTMLDivElement>,
@@ -33,6 +44,22 @@ const ThirdSection = () => {
         end: "top top",
       },
     });
+
+    if (window.innerWidth <= 900) {
+      gsap.to(right.current, {
+        x: "-0%",
+        duration: "5",
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: right.current,
+          scrub: 0.8,
+          start: "top bottom",
+          end: "top top",
+        },
+      });
+
+      return;
+    }
 
     if (window.innerWidth < 1400) {
       gsap.to(left.current, {
@@ -130,88 +157,179 @@ const ThirdSection = () => {
         OUR PARTNERS
       </div>
       <div className={styles.sponsorCon}>
-        <div className={styles.sponsor} id="lbank">
-          <div className={styles.sponsorDesc} ref={lbankTxt}>
-            {`LBank Cryptocurrency Exchange - is the world's leading crypto
+        {isSmallDevice ? (
+          <>
+            <div className={styles.sponsor} id="lbank" ref={lbankImg}>
+              <div className={styles.sponsorImg}>
+                <Image
+                  src={"/assets/images/partners/Lbank.svg"}
+                  alt="sponsor"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.sponsorDesc}>
+                {`LBank Cryptocurrency Exchange - is the world's leading crypto
             trading platform providing safe and professional services for global
             crypto users.`}
-          </div>
-          <div className={styles.sponsorImg} ref={lbankImg}>
-            <Image
-              src={"/assets/images/partners/Lbank.svg"}
-              alt="sponsor"
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
-        <div className={styles.sponsor} id="reef">
-          <div className={styles.sponsorDesc} ref={reefTxt}>
-            Reef chain is an EVM compatible blockchain for DeFi. It is fast,
-            scalable, has low transaction costs and does no wasteful mining.
-          </div>
-          <div className={styles.sponsorImg} ref={reefImg}>
-            <Image
-              src={"/assets/images/partners/Reef.svg"}
-              alt="sponsor"
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
-        <div className={styles.sponsor} id="patricia">
-          <div className={styles.sponsorDesc} ref={patriciaTxt}>
-            Patricia is an alternative payment solutions company that
-            facilitates the easy use of cryptocurrencies for everyday
-            transactions. By harnessing the intrinsic power of Blockchain
-            technology, Patricia is set to make Crypto easy for mankind, hereby
-            creating a new and sustainable path for the present and future
-            generations.
-          </div>
-          <div className={styles.sponsorImg} ref={patriciaImg}>
-            <Image
-              src={"/assets/images/partners/Patricia.svg"}
-              alt="sponsor"
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
-        <div className={styles.sponsor} id="dimen">
-          <div className={styles.sponsorDesc} ref={dimenTxt}>
-            Dimension11 Studios is an Indie game studio located in Lagos,
-            Ile-Ife, and Dubai. Our goal as an indie game studio is to enrich
-            the lives of everyday people by harnessing the latest technologies.
-            This agenda is advanced right from the heart of Africa and across
-            the world, by a legion of deeply creative, skilful and passionate
-            people.
-          </div>
-          <div className={styles.sponsorImg} ref={dimenImg}>
-            <Image
-              src={"/assets/images/partners/Dimension11.svg"}
-              alt="sponsor"
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
-        <div className={styles.sponsor} id="deca">
-          <div className={styles.sponsorDesc} ref={decaTxt}>
-            DECA4 brings together thought leaders and innovators to identify how
-            clients can leverage blockchain in their business. With experts from
-            software development and consulting, legal, marketing, educational,
-            business development organizations, we are ideally positioned to
-            assist you in building a blockchain solution which suits your needs.
-          </div>
-          <div className={styles.sponsorImg} ref={decaImg}>
-            <Image
-              src={"/assets/images/partners/deca4.svg"}
-              alt="sponsor"
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
+              </div>
+            </div>
+            <div className={styles.sponsor} id="reef" ref={reefImg}>
+              <div className={styles.sponsorImg}>
+                <Image
+                  src={"/assets/images/partners/Reef.svg"}
+                  alt="sponsor"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.sponsorDesc}>
+                Reef chain is an EVM compatible blockchain for DeFi. It is fast,
+                scalable, has low transaction costs and does no wasteful mining.
+              </div>
+            </div>
+            <div className={styles.sponsor} id="patricia" ref={patriciaImg}>
+              <div className={styles.sponsorImg}>
+                <Image
+                  src={"/assets/images/partners/Patricia.svg"}
+                  alt="sponsor"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.sponsorDesc}>
+                Patricia is an alternative payment solutions company that
+                facilitates the easy use of cryptocurrencies for everyday
+                transactions. By harnessing the intrinsic power of Blockchain
+                technology, Patricia is set to make Crypto easy for mankind,
+                hereby creating a new and sustainable path for the present and
+                future generations.
+              </div>
+            </div>
+            <div className={styles.sponsor} id="dimen" ref={dimenImg}>
+              <div className={styles.sponsorImg}>
+                <Image
+                  src={"/assets/images/partners/Dimension11.svg"}
+                  alt="sponsor"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.sponsorDesc}>
+                Dimension11 Studios is an Indie game studio located in Lagos,
+                Ile-Ife, and Dubai. Our goal as an indie game studio is to
+                enrich the lives of everyday people by harnessing the latest
+                technologies. This agenda is advanced right from the heart of
+                Africa and across the world, by a legion of deeply creative,
+                skilful and passionate people.
+              </div>
+            </div>
+            <div className={styles.sponsor} id="deca" ref={decaImg}>
+              <div className={styles.sponsorImg}>
+                <Image
+                  src={"/assets/images/partners/deca4.svg"}
+                  alt="sponsor"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.sponsorDesc}>
+                DECA4 brings together thought leaders and innovators to identify
+                how clients can leverage blockchain in their business. With
+                experts from software development and consulting, legal,
+                marketing, educational, business development organizations, we
+                are ideally positioned to assist you in building a blockchain
+                solution which suits your needs.
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.sponsor} id="lbank">
+              <div className={styles.sponsorDesc} ref={lbankTxt}>
+                {`LBank Cryptocurrency Exchange - is the world's leading crypto
+            trading platform providing safe and professional services for global
+            crypto users.`}
+              </div>
+              <div className={styles.sponsorImg} ref={lbankImg}>
+                <Image
+                  src={"/assets/images/partners/Lbank.svg"}
+                  alt="sponsor"
+                  width={400}
+                  height={400}
+                />
+              </div>
+            </div>
+            <div className={styles.sponsor} id="reef">
+              <div className={styles.sponsorDesc} ref={reefTxt}>
+                Reef chain is an EVM compatible blockchain for DeFi. It is fast,
+                scalable, has low transaction costs and does no wasteful mining.
+              </div>
+              <div className={styles.sponsorImg} ref={reefImg}>
+                <Image
+                  src={"/assets/images/partners/Reef.svg"}
+                  alt="sponsor"
+                  width={400}
+                  height={400}
+                />
+              </div>
+            </div>
+            <div className={styles.sponsor} id="patricia">
+              <div className={styles.sponsorDesc} ref={patriciaTxt}>
+                Patricia is an alternative payment solutions company that
+                facilitates the easy use of cryptocurrencies for everyday
+                transactions. By harnessing the intrinsic power of Blockchain
+                technology, Patricia is set to make Crypto easy for mankind,
+                hereby creating a new and sustainable path for the present and
+                future generations.
+              </div>
+              <div className={styles.sponsorImg} ref={patriciaImg}>
+                <Image
+                  src={"/assets/images/partners/Patricia.svg"}
+                  alt="sponsor"
+                  width={400}
+                  height={400}
+                />
+              </div>
+            </div>
+            <div className={styles.sponsor} id="dimen">
+              <div className={styles.sponsorDesc} ref={dimenTxt}>
+                Dimension11 Studios is an Indie game studio located in Lagos,
+                Ile-Ife, and Dubai. Our goal as an indie game studio is to
+                enrich the lives of everyday people by harnessing the latest
+                technologies. This agenda is advanced right from the heart of
+                Africa and across the world, by a legion of deeply creative,
+                skilful and passionate people.
+              </div>
+              <div className={styles.sponsorImg} ref={dimenImg}>
+                <Image
+                  src={"/assets/images/partners/Dimension11.svg"}
+                  alt="sponsor"
+                  width={400}
+                  height={400}
+                />
+              </div>
+            </div>
+            <div className={styles.sponsor} id="deca">
+              <div className={styles.sponsorDesc} ref={decaTxt}>
+                DECA4 brings together thought leaders and innovators to identify
+                how clients can leverage blockchain in their business. With
+                experts from software development and consulting, legal,
+                marketing, educational, business development organizations, we
+                are ideally positioned to assist you in building a blockchain
+                solution which suits your needs.
+              </div>
+              <div className={styles.sponsorImg} ref={decaImg}>
+                <Image
+                  src={"/assets/images/partners/deca4.svg"}
+                  alt="sponsor"
+                  width={400}
+                  height={400}
+                />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
